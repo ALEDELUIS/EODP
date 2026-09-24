f0fc9bd475c6baa6621ce2527a1e0ac6e1a312e0
ALEDELUIS <100451474@alumnos.uc3m.es>
2026-09-24 19:57:45 +0200

    mtf
    mtf

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -92,6 +92,21 @@ class mtf:
         :return fnAlt: 1D normalised frequencies 2D ALT (f/(1/w))
         """
         #TODO
+        fstepAlt = 1 / nlines / w  # fstepAlt: 333.3333333333333
+        fstepAct = 1 / ncolumns / w  # fstepAct: 222.22222222222223
+
+        eps = 1e-6
+        fAlt = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAlt)
+        fAct = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAct)
+
+        [fAltxx, fActxx] = np.meshgrid(fAlt, fAct, indexing='ij')  # Please use ‘ij’ indexing or you will get the transpose
+        f2D = np.sqrt(fAltxx * fAltxx + fActxx * fActxx)
+
+        fn2D = f2D / (1 / w)
+        fc = D / (lambd * focal)
+        fr2D = f2D / fc
+        fnAct = fAct / (1 / w)
+        fnAlt = fAlt / (1 / w)
         return fn2D, fr2D, fnAct, fnAlt
 
     def mtfDiffract(self,fr2D):
@@ -101,6 +116,12 @@ class mtf:
         :return: diffraction MTF
         """
         #TODO
+        # Inicializar la MTF
+        acos_vec = np.vectorize(np.arccos)
+
+        Hdiff = (2 / np.pi) * (acos_vec(fr2D) - fr2D * np.sqrt(1 - fr2D * fr2D))
+        Hdiff[fr2D * fr2D > 1] = 0
+
         return Hdiff
 
 
@@ -114,6 +135,9 @@ class mtf:
         :return: Defocus MTF
         """
         #TODO
+        x = np.pi * defocus * fr2D * (1 - fr2D)
+        Hdefoc = 2 * j1(x) / x
+
         return Hdefoc
 
     def mtfWfeAberrations(self, fr2D, lambd, kLF, wLF, kHF, wHF):
